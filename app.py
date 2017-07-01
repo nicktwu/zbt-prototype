089024fabf182c81fad0df62b13cfe27452f4e77
nwu <nwu@mit.edu>
2017-07-01 13:52:31 -0500

    Added midnight db model

--- app.py
+++ app.py
@@ -6,6 +6,25 @@ app = Flask(__name__)
 app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 
+
+class Midnight(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    day = db.Column(db.Date)
+    zebe = db.Column(db.String(20))
+    task = db.Column(db.String(80))
+    notes = db.Column(db.String(300))
+
+    def __init__(self, day, zebe, task, notes):
+        self.day = day
+        self.zebe = zebe
+        self.task = task
+        self.notes = notes
+
+
 @app.route('/')
 def hello_world():
     return 'Hello, you!'
+
+
+if __name__ == '__main__':
+    app.run()
