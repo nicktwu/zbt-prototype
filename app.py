8aeed1f242baddeecf66375883cb4ca072a09f7a
nwu <nwu@mit.edu>
2017-07-01 00:08:38 -0500

    Added sqlalchemy

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config.from_pyfile('config.py')
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
